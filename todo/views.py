aade3c51fdb716d21c76bc943ce16df5337fa073
mohotasir <mohotasir1036@gmail.com>
2024-10-11 20:09:04 +0600

    complete CRUD

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,44 @@
 from .models import Task
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 def mytodo(request):
-    tasks = Task.objects.all()
+    # Fetch tasks based on completion status
+    remaining_tasks = Task.objects.filter(isCompleted=False)
+    completed_tasks = Task.objects.filter(isCompleted=True)
+
     if request.method == 'POST':
-        new_task = Task(title=request.POST['title'])
-        new_task.save()
-    return render(request,'home.html', {'tasks' : tasks})
+        # Add new task
+        if 'title' in request.POST:
+            new_task = Task(title=request.POST['title'])
+            new_task.save()
+            return redirect('mytodo')
+
+        # Delete task
+        elif 'delete_task' in request.POST:
+            task_id = request.POST.get('task_id')
+            task = get_object_or_404(Task, id=task_id)
+            task.delete()
+            return redirect('mytodo')
+
+        # Toggle task completion
+        elif 'done' in request.POST:
+            task_id = request.POST.get('task_id')
+            task = get_object_or_404(Task, id=task_id)
+            task.isCompleted = not task.isCompleted  # Toggle the isCompleted status
+            task.save()
+            return redirect('mytodo')
+
+    return render(request, 'home.html', {
+        'remaining_tasks': remaining_tasks,
+        'completed_tasks': completed_tasks
+    })
+def update_task(request, id):
+    task = get_object_or_404(Task, id=id)
+
+    if request.method == 'POST':
+        # Update the task title
+        task.title = request.POST['title']
+        task.save()
+        return redirect('mytodo')
+
+    return render(request, 'home.html', {'task': task})
--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,9 +3,11 @@
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import mytodo
+from todo.views import mytodo,update_task
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',mytodo, name='mytodo'),
+    path('update-task/<int:id>/', update_task, name='update_task'),
+
     
 ]
